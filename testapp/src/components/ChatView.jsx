1f18b72ab8fd64b730ff091a306b7938c82cc34a
youshen52 <122770139+youshen52@users.noreply.github.com>
2023-12-08 14:58:15 +0800

    Added Active as well as Message Input

---
+++ testapp/src/components/ChatView.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import MessageList from './MessageList';
+import MessageInput from './MessageInput';
+
+function ChatView() {
+  const [messages, setMessages] = useState([
+    { text: 'Hi', sender: 'other' },
+    { text: 'Hello', sender: 'user' },
+  ]);
+
+  const handleSendMessage = (messageText) => {
+    const newMessage = { text: messageText, sender: 'user' };
+    setMessages([...messages, newMessage]);
+  };
+
+  return (
+    <div className="hidden lg:col-span-2 lg:block">
+      <div className="w-full">
+        <div className="relative flex items-center p-3 border-b border-gray-300">
+          <img className="object-cover w-10 h-10 rounded-full" src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
+          <span className="block ml-2 font-bold text-gray-600">Emma</span>
+          <span className="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3"></span>
+        </div>
+        <MessageList messages={messages} />
+        <MessageInput onSendMessage={handleSendMessage} />
+      </div>
+    </div>
+  );
+}
+
+export default ChatView;
---
+++ testapp/src/components/MessageList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MessageList({ messages }) {
+  return (
+    <div className="relative w-full p-6 overflow-y-auto h-[40rem]">
+      <ul className="space-y-2">
+        {messages.map((message, index) => (
+          <li key={index} className={`flex justify-${message.sender === 'user' ? 'end' : 'start'}`}>
+            <div className={`relative max-w-xl px-4 py-2 text-gray-700 ${message.sender === 'user' ? 'bg-gray-100 rounded' : 'rounded shadow'}`}>
+              <span className="block">{message.text}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default MessageList;
