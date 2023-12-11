9ec41e22dc15ac9086c54fb4542f76d4cb276696
youshen52 <122770139+youshen52@users.noreply.github.com>
2023-12-11 22:45:57 +0800

    Added Emoji Support

--- testapp/src/components/MessageInput.jsx
+++ testapp/src/components/MessageInput.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react';
-
+import EmojiPicker from 'emoji-picker-react';
 
 function MessageInput({ onSendMessage }) {
-    const [messageText, setMessageText] = useState('');
+  const [messageText, setMessageText] = useState('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
-    const handleInputChange = (e) => {
-        setMessageText(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    setMessageText(e.target.value);
+  };
 
-    const handleSendMessage = (e) => {
-        e.preventDefault();
-        if (messageText.trim() !== '') {
-            onSendMessage(messageText);
-            setMessageText('');
-        }
-    };
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (messageText.trim() !== '') {
+      onSendMessage(messageText);
+      setMessageText('');
+    }
+  };
+
+  const handleEmojiClick = (emojiData) => {
+    setMessageText((prevMessage) => prevMessage + emojiData.emoji);
+    setShowEmojiPicker(false);
+  };
 
   return (
     <form onSubmit={handleSendMessage}>
-    <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
-      <button className='bg-gray-800 text-2xl px-2 py-1 mr-1'>
-        <ion-icon name="happy-outline"></ion-icon>
-      </button>
-      <button className='bg-gray-800 text-2xl px-2 py-1'>
-        <ion-icon name="attach-outline"></ion-icon>
-      </button>
-      <input
+      <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+        <button
+          className="bg-gray-800 text-2xl px-2 py-1 mr-1"
+          onClick={() => setShowEmojiPicker((prev) => !prev)}
+        >
+          <ion-icon name="happy-outline"></ion-icon>
+        </button>
+        {showEmojiPicker && (
+          <EmojiPicker onEmojiClick={handleEmojiClick} autoFocusSearch />
+        )}
+        <button className="bg-gray-800 text-2xl px-2 py-1">
+          <ion-icon name="attach-outline"></ion-icon>
+        </button>
+        <input
           type="text"
           placeholder="Message"
           value={messageText}
@@ -34,10 +46,10 @@ function MessageInput({ onSendMessage }) {
           name="message"
           required
         />
-      <button type="submit" className='text-gray-800 bg-transparent text-2xl px-2 py-1'>
-        <ion-icon name="send"></ion-icon>
-      </button>
-    </div>
+        <button type="submit" className="text-gray-800 bg-transparent text-2xl px-2 py-1">
+          <ion-icon name="send"></ion-icon>
+        </button>
+      </div>
     </form>
   );
 }
