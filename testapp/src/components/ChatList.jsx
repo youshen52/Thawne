9cc5b9f53256e5117e3f3209b816d8adcde895e9
youshen52 <122770139+youshen52@users.noreply.github.com>
2023-12-12 08:21:00 +0800

    Fix Warning: Unique key prop

--- testapp/src/components/ChatList.jsx
+++ testapp/src/components/ChatList.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 
-// function ChatList(props) {
-  //  const chats = props.chats
-
 function ChatList({ onChatSelect }) {
   const chats = [
     { name: 'Lewis', message: 'kukubird' },
@@ -20,7 +17,6 @@ function ChatList({ onChatSelect }) {
     { name: 'Boon Ping', message: 'The glaze is real' }
   ];
 
-
   const [activeChat, setActiveChat] = useState(null);
 
   const handleChatClick = (index) => {
@@ -31,35 +27,33 @@ function ChatList({ onChatSelect }) {
 
   return (
     <>
-
-<div className="overflow-auto h-[47rem]">
-  <h2 className="my-2 mb-4 ml-2 text-lg text-gray-600 font-semibold">Chats ({chats.length})</h2>
-  {chats.map((chat, index) => (
-    <div
-      className={`rounded-xl cursor-pointer transition duration-300 ease-in-out ${
-        activeChat === index ? "bg-gray-100" : "hover:bg-gray-50"
-      }`}
-      key={chat.id}
-      onClick={() => handleChatClick(index)}
-    >
-      <div className="flex items-center px-4 py-3">
-        <img
-          className="object-cover w-12 h-12 rounded-full mr-4"
-          src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-          alt="username"
-        />
-        <div className="flex-1">
-          <div className="flex justify-between items-center">
-            <span className="font-semibold text-gray-800">{chat.name}</span>
-            <span className="text-sm text-gray-600">25 minutes</span>
+      <div className="overflow-auto h-[47rem]">
+        <h2 className="my-2 mb-4 ml-2 text-lg text-gray-600 font-semibold">Chats ({chats.length})</h2>
+        {chats.map((chat, index) => (
+          <div
+            className={`rounded-xl cursor-pointer transition duration-300 ease-in-out ${
+              activeChat === index ? "bg-gray-100" : "hover:bg-gray-50"
+            }`}
+            key={index} // Use the index as a key
+            onClick={() => handleChatClick(index)}
+          >
+            <div className="flex items-center px-4 py-3">
+              <img
+                className="object-cover w-12 h-12 rounded-full mr-4"
+                src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
+                alt="username"
+              />
+              <div className="flex-1">
+                <div className="flex justify-between items-center">
+                  <span className="font-semibold text-gray-800">{chat.name}</span>
+                  <span className="text-sm text-gray-600">25 minutes</span>
+                </div>
+                <span className="text-sm text-gray-600">{chat.message}</span>
+              </div>
+            </div>
           </div>
-          <span className="text-sm text-gray-600">{chat.message}</span>
-        </div>
+        ))}
       </div>
-    </div>
-  ))}
-</div>
-
     </>
   );
 }
