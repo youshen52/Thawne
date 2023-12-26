9e9e52ae2b6a53418670464a60bb8675c90ab33b
Lewis <lewistay03@gmail.com>
2023-12-26 15:28:55 +0800

    message list

--- thawne/src/api/chatApi.js
+++ thawne/src/api/chatApi.js
@@ -67,38 +67,38 @@ async function submitMessage(content) {
 }
 
 
-async function getMessageList(currentChat) {
-  try {
-    const response = await fetch(API_CONFIG.endpoints.getTopMessages, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(currentChat),
-    }).then((response) => response.json());
-      console.log(response)
-      if (response.success){
-        console.log(response.message)
-        return response.message
-      }
-      else{
-        console.log(response)
-      }
+// async function getMessageList(currentChat) {
+//   try {
+//     const response = await fetch(API_CONFIG.endpoints.getTopMessages, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(currentChat),
+//     }).then((response) => response.json());
+//       console.log(response)
+//       if (response.success){
+//         console.log(response.message)
+//         return response.message
+//       }
+//       else{
+//         console.log(response)
+//       }
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch message list: ${response.status}`);
-    }
-    else{
-      console.log(response)
-      return response.message
-    }
+//     if (!response.ok) {
+//       throw new Error(`Failed to fetch message list: ${response.status}`);
+//     }
+//     else{
+//       console.log(response)
+//       return response.message
+//     }
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching message list:', error.message);
-    throw error;
-  }
-}
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching message list:', error.message);
+//     throw error;
+//   }
+// }
 
-export { reflectAllChats, createChat, submitMessage, getMessageList };
+export { reflectAllChats, createChat, submitMessage };
--- thawne/src/components/ChatView/MessageList.jsx
+++ thawne/src/components/ChatView/MessageList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import useToken from '../../hooks/useToken';
 
 function MessageList({ messages }) {
 
+  const { token } = useToken();
   const checkMessageList = (messages) => {
     if (messages.length == 0){
       return <p className='text-white text-center'>Chat does not have any messages yet.</p>
@@ -11,6 +12,16 @@ function MessageList({ messages }) {
       null
     }
   }
+
+  const userMessage = (senderId) => {
+    if (senderId === token){
+      return true
+    }
+    else{
+      false
+    }
+  }
+
   return (
     <div className="relative w-full p-6 overflow-y-auto h-[40rem] " style={{ 
       backgroundImage: `url("/images/chatWallpaper.jpg")`
@@ -18,9 +29,9 @@ function MessageList({ messages }) {
       {checkMessageList(messages)}
       <ul className="space-y-2">
         {messages.map((message, index) => (
-          <li key={index} className={`flex justify-${message.sender === 'user' ? 'end' : 'start'}`}>
-            <div className={`relative max-w-xl px-4 py-2 text-white bg-gray-700  ${message.sender === 'user' ? 'bg-gray-100 rounded' : 'rounded shadow'}`}>
-              <span className="block">{message.content}</span>
+          <li key={index} className={`flex justify-${userMessage(Object.keys(message.sent_from)[0]) ? 'end' : 'start'}`}>
+            <div className={`relative max-w-xl px-4 py-2 text-white bg-gray-700  ${userMessage(Object.keys(message.sent_from)[0]) ? 'bg-gray-100 rounded' : 'rounded shadow'}`}>
+              {userMessage(Object.keys(message.sent_from)[0]) ? <span className="block">{message.content}</span> : <span className="block">{message.content}</span>}
             </div>
           </li>
         ))}
