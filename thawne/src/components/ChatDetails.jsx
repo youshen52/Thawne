06f23f913df871a24f92d4ebf0a72a63feff8032
youshen52 <122770139+youshen52@users.noreply.github.com>
2024-01-07 15:15:53 +0800

    jkj

--- thawne/src/components/ChatDetails.jsx
+++ thawne/src/components/ChatDetails.jsx
@@ -1,118 +1,123 @@
 import React from 'react';
+import { Transition } from '@headlessui/react';
 import FileCarousel from './ChatView/FileCarousel';
 
-function ChatDetails({ chatDetails, onClose }) {
+function ChatDetails({ chatDetails, isDetailsOpen, onClose }) {
     return (
-        <div className="absolute top-0 right-0 bottom-0 w-1/3 bg-zinc-800 shadow-md p-4 overflow-y-auto max-h-full">
-    <div className="flex justify-between items-center">
-    <h2 className="text-lg font-semibold mb-4 text-white">Info</h2>
-    <button onClick={onClose} className="text-white px-4 py-2 rounded-md text-lg">
-        <ion-icon name="close"></ion-icon>
-    </button>
-    </div>
-    <div className="flex flex-col h-full space-y-4">
-    <div className="flex-grow ">
-    <img
-                className="object-cover w-3/4 rounded-full p-5 "
-                src="/images/default_pfp.png"
-                alt="username"
-              />
-        <p className='italic mb-2 text-white'>Created by: Creator XX at XX/XX/XX </p>
 
-    </div>
-        <div className="flex-grow">
-        <div class="grid py-6 sm:grid-cols-2">
-        <div class="text-white">
-          <h2 class="text-lg font-semibold leading-4">Chat Details</h2>
+        
 
-          <p class=" mt-5">Chat ID: </p>
+                    <div className="absolute top-0 right-0 bottom-0 w-1/3 bg-zinc-800 shadow-md p-4 overflow-y-auto max-h-full">
+                    <div className="flex justify-between items-center">
+                    <h2 className="text-lg font-semibold mb-4 text-white">Info</h2>
+                    <button onClick={onClose} className="text-white px-4 py-2 rounded-md text-lg">
+                        <ion-icon name="close"></ion-icon>
+                    </button>
+                    </div>
+                    <div className="flex flex-col h-full space-y-4">
+                    <div className="flex-grow ">
+                    <img
+                                className="object-cover w-3/4 rounded-full p-5 "
+                                src="/images/default_pfp.png"
+                                alt="username"
+                            />
+                        <p className='italic mb-2 text-white'>Created by: Creator XX at XX/XX/XX </p>
+                
+                    </div>
+                        <div className="flex-grow">
+                        <div className="grid py-6 sm:grid-cols-2">
+                        <div className="text-white">
+                            <h2 className="text-lg font-semibold leading-4">Chat Details</h2>
+                
+                            <p className=" mt-5">Chat ID: </p>
+                
+                            <p className="mt-6">Chat Security:</p>
+                
+                        </div>
+                        <div className="mt-4 flex items-center sm:justify-end">
+                            <div className="flex flex-col gap-3 text-white italic">
+                            
+                            <span className='mt-3 relative inline-flex cursor-pointer items-center'>No Hair</span>
+                            <span className='mt-3 text-xs bg-red-600 text-white font-semibold p-1 rounded-md'>Top Secret</span>
+                
+                
+                          </div>
+                        </div>
+                </div>
+                
+                        </div>
+                
+                
+                        <div className="flex-grow">
+                        <div className="overflow-y-auto text-white p-4">
+                            <div className="flex items-center">
+                            <p className="mb-2">Chat Description: {chatDetails.description}</p>
+                            <button className="ml-auto text-white">
+                                <ion-icon name="create-outline"></ion-icon>
+                            </button>
+                            </div>
+                
+                
+                        </div>
+                        </div>
+                
+                        <div className="flex-grow">
+                        <h2 className="text-lg font-semibold mb-4 text-white">Files</h2>
+                        <FileCarousel />
+                        </div>
+                
+                        <div className="flex-grow">
+                        <h2 className="text-lg font-semibold mb-4 text-white">Participants</h2>
+                        <div className="flex items-center mb-3">
+                        <img
+                                    className="object-cover w-12 h-12 rounded-full mr-4"
+                                    src="/images/default_pfp.png"
+                                    alt="username"
+                                />
+                            <div>
+                            <p className="font-semibold text-white">User 1</p>
+                            <p className="text-gray-500 text-sm">ID: 001</p>
+                            </div>
+                        </div>
+                
+                        <div className="flex items-center mb-3">
+                        <img
+                                    className="object-cover w-12 h-12 rounded-full mr-4"
+                                    src="/images/default_pfp.png"
+                                    alt="username"
+                                />
+                            <div>
+                            <p className="font-semibold text-white">User 2</p>
+                            <p className="text-gray-500 text-sm">ID: 002</p>
+                            </div>
+                        </div>
+                
+                        <div className="flex items-center mb-3">
+                        <img
+                                    className="object-cover w-12 h-12 rounded-full mr-4"
+                                    src="/images/default_pfp.png"
+                                    alt="username"
+                                />
+                            <div>
+                            <p className="font-semibold text-white">User 3</p>
+                            <p className="text-gray-500 text-sm">ID: 003</p>
+                            </div>
+                        </div>
+                        </div>
+                
+                        <div className="flex-grow">
+                            <button className='text-red-500 w-full px-4 py-2 '>
+                                <ion-icon name="exit-outline"></ion-icon> Exit Group
+                            </button>
+                            <button className='text-red-500 w-full px-4 py-2'>
+                                <ion-icon name="megaphone-outline"></ion-icon> Report Group
+                            </button>
+                            
+                        </div>
+                    </div>
+                    </div>
 
-          <p class="mt-6">Chat Security:</p>
-          
 
-        </div>
-        <div class="mt-4 flex items-center sm:justify-end">
-          <div class="flex flex-col gap-3 text-white italic">
-            
-            <span className='mt-3 relative inline-flex cursor-pointer items-center'>No Hair</span>
-            <span className='mt-3 text-xs bg-red-600 text-white font-semibold p-1 rounded-md'>Top Secret</span>
-
-
-          </div>
-        </div>
-</div>
-
-        </div>
-
-
-        <div className="flex-grow">
-        <div className="overflow-y-auto text-white p-4">
-            <div className="flex items-center">
-            <p className="mb-2">Chat Description: {chatDetails.description}</p>
-            <button className="ml-auto text-white">
-                <ion-icon name="create-outline"></ion-icon>
-            </button>
-            </div>
-
-
-        </div>
-        </div>
-
-        <div className="flex-grow">
-        <h2 className="text-lg font-semibold mb-4 text-white">Files</h2>
-        <FileCarousel />
-        </div>
-
-        <div className="flex-grow">
-        <h2 className="text-lg font-semibold mb-4 text-white">Participants</h2>
-        <div className="flex items-center mb-3">
-        <img
-                    className="object-cover w-12 h-12 rounded-full mr-4"
-                    src="/images/default_pfp.png"
-                    alt="username"
-                />
-            <div>
-            <p className="font-semibold text-white">User 1</p>
-            <p className="text-gray-500 text-sm">ID: 001</p>
-            </div>
-        </div>
-
-        <div className="flex items-center mb-3">
-        <img
-                    className="object-cover w-12 h-12 rounded-full mr-4"
-                    src="/images/default_pfp.png"
-                    alt="username"
-                />
-            <div>
-            <p className="font-semibold text-white">User 2</p>
-            <p className="text-gray-500 text-sm">ID: 002</p>
-            </div>
-        </div>
-
-        <div className="flex items-center mb-3">
-        <img
-                    className="object-cover w-12 h-12 rounded-full mr-4"
-                    src="/images/default_pfp.png"
-                    alt="username"
-                />
-            <div>
-            <p className="font-semibold text-white">User 3</p>
-            <p className="text-gray-500 text-sm">ID: 003</p>
-            </div>
-        </div>
-        </div>
-
-        <div className="flex-grow">
-            <button className='text-red-500 w-full px-4 py-2 '>
-                <ion-icon name="exit-outline"></ion-icon> Exit Group
-            </button>
-            <button className='text-red-500 w-full px-4 py-2'>
-                <ion-icon name="megaphone-outline"></ion-icon> Report Group
-            </button>
-            
-        </div>
-    </div>
-    </div>
 
     );
 }
--- thawne/src/components/ChatView.jsx
+++ thawne/src/components/ChatView.jsx
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import MessageList from './ChatView/MessageList';
-import MessageInput from './ChatView/MessageInput';
-import ChatDetails from './ChatDetails';
-import useToken from '../hooks/useToken';
-import API_CONFIG from '../config/api';
+import React, { useState, useEffect } from "react";
+import MessageList from "./ChatView/MessageList";
+import MessageInput from "./ChatView/MessageInput";
+import ChatDetails from "./ChatDetails";
+import useToken from "../hooks/useToken";
+import API_CONFIG from "../config/api";
 
-function ChatView({ selectedChat, chatList, currentChatInfo, isDetailsOpen, setDetailsOpen }) {
+function ChatView({
+  selectedChat,
+  chatList,
+  currentChatInfo,
+  isDetailsOpen,
+  setDetailsOpen,
+}) {
   const [messages, setMessages] = useState([]);
   const { token } = useToken();
-  
 
   useEffect(() => {
     if (selectedChat) {
@@ -22,9 +27,9 @@ function ChatView({ selectedChat, chatList, currentChatInfo, isDetailsOpen, setD
       const getMessageList = async (currentChat) => {
         try {
           const response = await fetch(API_CONFIG.endpoints.getTopMessages, {
-            method: 'POST',
+            method: "POST",
             headers: {
-              'Content-Type': 'application/json',
+              "Content-Type": "application/json",
             },
             body: JSON.stringify(currentChat),
           }).then((response) => response.json());
@@ -37,7 +42,7 @@ function ChatView({ selectedChat, chatList, currentChatInfo, isDetailsOpen, setD
             throw new Error(`Failed to fetch message list: ${response.status}`);
           }
         } catch (error) {
-          console.error('Error fetching message list:', error.message);
+          console.error("Error fetching message list:", error.message);
           throw error;
         }
       };
@@ -49,47 +54,73 @@ function ChatView({ selectedChat, chatList, currentChatInfo, isDetailsOpen, setD
   }, [selectedChat]);
 
   const checkSecurity = (level) => {
-    if (level === 'Top Secret') {
-      return <span className='ml-2 text-xs bg-red-600 text-white font-semibold p-1 rounded-md'>Top Secret</span>;
-    } else if (level === 'Sensitive') {
-      return <span className='ml-2 text-xs bg-yellow-600 text-white font-semibold p-1 rounded-md'>Sensitive</span>;
+    if (level === "Top Secret") {
+      return (
+        <span className="ml-2 text-xs bg-red-600 text-white font-semibold p-1 rounded-md">
+          Top Secret
+        </span>
+      );
+    } else if (level === "Sensitive") {
+      return (
+        <span className="ml-2 text-xs bg-yellow-600 text-white font-semibold p-1 rounded-md">
+          Sensitive
+        </span>
+      );
     } else {
-      return <span className='ml-2 text-xs bg-green-600 text-white font-semibold p-1 rounded-md'>Open</span>;
+      return (
+        <span className="ml-2 text-xs bg-green-600 text-white font-semibold p-1 rounded-md">
+          Open
+        </span>
+      );
     }
   };
-
-  
 
   return (
     <div className="lg:col-span-2 lg:block">
       {selectedChat ? (
-        <div className={`w-full h-3/4 ${isDetailsOpen ? 'lg:w-2/3' : 'lg:w-full'}`}>
-          <div className="relative flex items-center p-3 border-b border-black bg-zinc-800"
-                    onClick={() => setDetailsOpen(!isDetailsOpen)}
+        <div
+          className={`w-full h-3/4 ${
+            isDetailsOpen ? "lg:w-2/3" : "lg:w-full"
+          } transition-all duration-500`}
+        >
+          <div
+            className="relative flex items-center p-3 border-b border-black bg-zinc-800"
+            onClick={() => setDetailsOpen(!isDetailsOpen)}
           >
-            <img className="object-cover w-10 h-10 rounded-full" src="/images/default_pfp.png" alt="username" />
-            <span className="block ml-2 font-bold text-white">{selectedChat.chat_name}</span>
+            <img
+              className="object-cover w-10 h-10 rounded-full"
+              src="/images/default_pfp.png"
+              alt="username"
+            />
+            <span className="block ml-2 font-bold text-white">
+              {selectedChat.chat_name}
+            </span>
             <span className="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3"></span>
             {checkSecurity(selectedChat.security_level)}
           </div>
           <MessageList messages={messages} />
-          <div className='bg-zinc-800'>
-            <MessageInput chatList={chatList} currentChatInfo={currentChatInfo} />
+          <div className="bg-zinc-800">
+            <MessageInput
+              chatList={chatList}
+              currentChatInfo={currentChatInfo}
+            />
           </div>
         </div>
       ) : (
-        <p className="text-center text-gray-500">Select a chat to start messaging.</p>
+        <p className="text-center text-gray-500">
+          Select a chat to start messaging.
+        </p>
       )}
 
       {isDetailsOpen && (
         <ChatDetails
           chatDetails={selectedChat}
+          isDetailsOpen={isDetailsOpen}
           onClose={() => setDetailsOpen(false)}
         />
       )}
     </div>
   );
-  
 }
 
 export default ChatView;
